dc2d65da5fc32503b8541e89eb9d82b6267abcdd
Fady <fadyderias_92@yahoo.com>
2017-12-20 22:57:48 +0200

    FIX: Removing typo

--- Tweet/UI/View Controllers/Authentication/LoginViewController.swift
+++ Tweet/UI/View Controllers/Authentication/LoginViewController.swift
@@ -16,7 +16,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loginView = LoginView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
-        loginView.signInButton.addTarget(self, action: #selector(loginUserViaTwitter), for: .touchUpInside)UI
+        loginView.signInButton.addTarget(self, action: #selector(loginUserViaTwitter), for: .touchUpInside)
         self.view.addSubview(loginView)
     }
     
