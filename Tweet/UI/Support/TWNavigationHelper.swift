9eb790025dfe121ae3ce724125f397ea48e4161b
Fady <fadyderias_92@yahoo.com>
2017-12-20 20:56:05 +0200

    FEAT: Creating TWNavigationHelper to manage user navigation when opening the application (based on user authentication status).

---
+++ Tweet/UI/Support/TWNavigationHelper.swift
@@ -0,0 +1,16 @@
+//
+//  TWNavigationHelper.swift
+//  Tweet
+//
+//  Created by Fady on 12/20/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import UIKit
+
+class TWNavigationHelper {
+    
+    static let sharedInstance = TWNavigationHelper()
+    
+    
+}
