810d698abe6a63f545905722caf9d85cb3e0e967
Fady <fadyderias_92@yahoo.com>
2017-12-20 20:39:39 +0200

    FEAT: Initial Project commit. Deleting storyboard and removing in from the project's plist file (Application is to be developed on programmatic basis).

---
+++ Tweet/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Tweet
+//
+//  Created by Fady on 12/20/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Tweet/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Tweet
+//
+//  Created by Fady on 12/20/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
