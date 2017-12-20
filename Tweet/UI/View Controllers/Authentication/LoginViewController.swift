3b4bd471be7398ad41c1e9f0ccfd69ce7a05e5f2
Fady <fadyderias_92@yahoo.com>
2017-12-20 22:53:59 +0200

    FEAT: Adding LoginView as subview in LoginViewController among setting a selector (function) to the LoginView sign in button.

--- Tweet/UI/View Controllers/Authentication/LoginViewController.swift
+++ Tweet/UI/View Controllers/Authentication/LoginViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
+    
+    var loginView: LoginView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loginView = LoginView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
+        loginView.signInButton.addTarget(self, action: #selector(loginUserViaTwitter), for: .touchUpInside)UI
+        self.view.addSubview(loginView)
+    }
+    
+    @objc func loginUserViaTwitter() {
+        print("User Logging in via twitter .. ")
     }
 }
 
