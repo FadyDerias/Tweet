2502e0287ff41296fc7c2a1960aa308df8dd860a
Fady <fadyderias_92@yahoo.com>
2017-12-20 21:29:49 +0200

    FEAT: Setting application's window to be visible, progress hud appearance, and setting the application's root view controller to be the login view controller.

--- Tweet/AppDelegate.swift
+++ Tweet/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        //Application does not support story boards, and is all programmatic
+        //Setting the window frame
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        setupAppearance()
+        TWNavigationHelper.sharedInstance.setRootViewControllerForUserAuthentication()
         return true
     }
     
