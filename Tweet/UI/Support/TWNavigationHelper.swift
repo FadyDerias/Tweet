49f7d3ba890570354afba8be11b042eff68b3496
Fady <fadyderias_92@yahoo.com>
2017-12-21 15:54:05 +0200

    FEAT: Adding function in TWNavigationHelper to navigate the user to the Followers Table View Controller after successful sign in.

--- Tweet/UI/Support/TWNavigationHelper.swift
+++ Tweet/UI/Support/TWNavigationHelper.swift
@@ -11,13 +11,19 @@ import UIKit
 class TWNavigationHelper {
     
     static let sharedInstance = TWNavigationHelper()
+    private let appWindow = AppDelegate.getAppDelegate().window
     
     func setRootViewControllerForUserAuthentication() {
-        let appWindow = AppDelegate.getAppDelegate().window
         let loginViewController = LoginViewController()
         let rootViewController = UINavigationController(rootViewController: loginViewController)
         rootViewController.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         rootViewController.navigationBar.shadowImage = UIImage()
+        appWindow?.rootViewController = rootViewController
+    }
+    
+    func setRootViewControllerForSuccessfulLogin() {
+        let followersViewController = FollowersTableViewController()
+        let rootViewController = UINavigationController(rootViewController: followersViewController)
         appWindow?.rootViewController = rootViewController
     }
 }
