16fb46af0a3a32e6420d0fb1e3afb022e32b08e3
Fady <fadyderias_92@yahoo.com>
2017-12-21 12:01:10 +0200

    FEAT: Creating FollowerTableViewCell with initial implementation.

---
+++ Tweet/UI/Views/FollowerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FollowerTableViewCell.swift
+//  Tweet
+//
+//  Created by Fady on 12/21/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import UIKit
+
+class FollowerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
