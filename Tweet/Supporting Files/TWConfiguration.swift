40fbe84924e446b2b009f1345c492097361e3e30
Fady <fadyderias_92@yahoo.com>
2017-12-22 17:01:08 +0200

    FEAT: Creating TWConfiguration that includes TwitterKeys struct that contain Twitter consumer key and consumerSecret.

---
+++ Tweet/Supporting Files/TWConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  TWConfiguration.swift
+//  Tweet
+//
+//  Created by Fady on 12/22/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import Foundation
+
+struct TwitterKeys {
+    static let consumerKey = "t75babjgI2NccBy3aLd8oqO2d"
+    static let consumderSecret = "6JU5iPqarg2Uy0TmnTJqDYUfGYrxowpRZVxH241leRqnkyx9QG"
+}
