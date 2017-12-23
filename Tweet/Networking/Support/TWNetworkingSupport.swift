8074c0e6d208621973c30fafa66beecbc9b2c583
Fady <fadyderias_92@yahoo.com>
2017-12-23 18:30:59 +0200

    FEAT: Creating TWNetworkingSupport with TWNetworkingConstants struct that holds the base url string.

---
+++ Tweet/Networking/Support/TWNetworkingSupport.swift
@@ -0,0 +1,14 @@
+//
+//  TWNetworkingSuport.swift
+//  Tweet
+//
+//  Created by Fady on 12/22/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import Foundation
+
+struct TWNetworkingConstants {
+    
+    static let baseUrl = "https://api.twitter.com/1.1"
+}
