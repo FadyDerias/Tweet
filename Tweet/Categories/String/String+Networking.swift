5769fb7bf7b1cb0cfdf3552e050d741959799620
Fady <fadyderias_92@yahoo.com>
2017-12-23 18:22:47 +0200

    FEAT: Creating String+Networking Category that includes the following functions:- - Function to hash a string via HMACAlgorithm. - Function that generates a random alphanumeric string - Function that generates the signature base string as required by Twitter Developer Docs - Function that generates the networking header string for the authorization header - Function that implies RFC3986 on a string - HMAC algorithm enum - function that does hash a string based on the requested type of HMAC algorithm

---
+++ Tweet/Categories/String/String+Networking.swift
@@ -0,0 +1,115 @@
+//
+//  String+Networking.swift
+//  Tweet
+//
+//  Created by Fady on 12/22/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func hmac(algorithm: HMACAlgorithm, key: String) -> String {
+        let cKey = key.cString(using: String.Encoding.utf8)
+        let cData = self.cString(using: String.Encoding.utf8)
+        var result = [CUnsignedChar](repeating: 0, count: Int(algorithm.digestLength()))
+        CCHmac(algorithm.toCCHmacAlgorithm(), cKey!, Int(strlen(cKey!)), cData!, Int(strlen(cData!)), &result)
+        let hmacData:NSData = NSData(bytes: result, length: (Int(algorithm.digestLength())))
+        let hmacBase64 = hmacData.base64EncodedString(options: NSData.Base64EncodingOptions.lineLength76Characters)
+        return String(hmacBase64)
+    }
+    
+    static func randomString(_ length: Int) -> String {
+        
+        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let len = UInt32(letters.length)
+        
+        var randomString = ""
+        
+        for _ in 0 ..< length {
+            let rand = arc4random_uniform(len)
+            var nextChar = letters.character(at: Int(rand))
+            randomString += NSString(characters: &nextChar, length: 1) as String
+        }
+        
+        return randomString
+    }
+    
+    static func generateSignatureBaseString(networkingParameters: [NetworkParameter]) -> String {
+        var result = ""
+        
+        for (index,parameter) in networkingParameters.enumerated() {
+            if (index != 0) {
+                result = String(format: "%@&%@=%@", result, parameter.key, parameter.value)
+            } else {
+                result = String(format: "%@=%@", parameter.key, parameter.value)
+            }
+        }
+        return result
+    }
+    
+    static func generateNetworkingHeaderString(networkingParameters: [NetworkParameter]) -> String {
+        var result = ""
+
+        for (index,parameter) in networkingParameters.enumerated() {
+            if (index != 0) {
+                result = String(format: "%@, %@=\"%@\"", result, parameter.key, parameter.value)
+            } else {
+                result = String(format: "%@=\"%@\"", parameter.key, parameter.value)
+            }
+        }
+        
+        let endResult = String(format: "OAuth %@",result)
+        return endResult
+    }
+    
+    func stringByAddingPercentEncodingForRFC3986() -> String? {
+        let unreserved = "-._~/?"
+        let allowed = NSMutableCharacterSet.alphanumeric()
+        allowed.addCharacters(in: unreserved)
+        return self.addingPercentEncoding(withAllowedCharacters: allowed as CharacterSet)
+    }
+}
+
+
+enum HMACAlgorithm {
+    case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+    
+    func toCCHmacAlgorithm() -> CCHmacAlgorithm {
+        var result: Int = 0
+        switch self {
+        case .MD5:
+            result = kCCHmacAlgMD5
+        case .SHA1:
+            result = kCCHmacAlgSHA1
+        case .SHA224:
+            result = kCCHmacAlgSHA224
+        case .SHA256:
+            result = kCCHmacAlgSHA256
+        case .SHA384:
+            result = kCCHmacAlgSHA384
+        case .SHA512:
+            result = kCCHmacAlgSHA512
+        }
+        return CCHmacAlgorithm(result)
+    }
+    
+    func digestLength() -> Int {
+        var result: CInt = 0
+        switch self {
+        case .MD5:
+            result = CC_MD5_DIGEST_LENGTH
+        case .SHA1:
+            result = CC_SHA1_DIGEST_LENGTH
+        case .SHA224:
+            result = CC_SHA224_DIGEST_LENGTH
+        case .SHA256:
+            result = CC_SHA256_DIGEST_LENGTH
+        case .SHA384:
+            result = CC_SHA384_DIGEST_LENGTH
+        case .SHA512:
+            result = CC_SHA512_DIGEST_LENGTH
+        }
+        return Int(result)
+    }
+}
