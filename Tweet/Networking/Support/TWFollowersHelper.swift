d51cd1443cf195b1109b98827fea4a08d9767d14
Fady <fadyderias_92@yahoo.com>
2017-12-24 01:02:04 +0200

    FEAT: Creating TWFollowersHelper to manage caching/retrieving/deleting the user's retrieved follower objects.

---
+++ Tweet/Networking/Support/TWFollowersHelper.swift
@@ -0,0 +1,16 @@
+//
+//  TWFollowersHelper.swift
+//  Tweet
+//
+//  Created by Fady on 12/24/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import Foundation
+
+class TWFollowersHelper {
+    
+    static let sharedInstance = TWFollowersHelper()
+    
+    
+}
