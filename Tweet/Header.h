cdaad979b054793486a2a07a536501b34f47d300
Fady <fadyderias_92@yahoo.com>
2017-12-23 18:03:08 +0200

    FEAT: Adding bridging header for project.

---
+++ Tweet/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  Tweet
+//
+//  Created by Fady on 12/22/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
+#import <CommonCrypto/CommonHMAC.h>
