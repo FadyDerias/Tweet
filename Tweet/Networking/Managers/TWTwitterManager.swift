9cf07ec629b995552c1277b25c3658dde0fdc784
Fady <fadyderias_92@yahoo.com>
2017-12-24 01:11:33 +0200

    FEAT: creating a function that does retrieve the user's followers via the user's id. Function includes parsing JSON and returning the array of followers in the success completion handler.

--- Tweet/Networking/Managers/TWTwitterManager.swift
+++ Tweet/Networking/Managers/TWTwitterManager.swift
@@ -7,10 +7,24 @@
 //
 
 import Foundation
-import TwitterKit
 import Swifter
+import Mapper
 
 class TWTwitterManager {
     
-
+    static let sharedInstance = TWTwitterManager()
+    private let swifter = Swifter(consumerKey: TwitterKeys.consumerKey, consumerSecret: TwitterKeys.consumerSecret)
+    let user = TWUsersHelper.sharedInstance.retrieveUserFromUserDefaults()!
+    
+    func getUserFollowers(cursor: String, _ success: @escaping (_ followers: [TWFollower],_ nextCursor: String) -> Void, failure: @escaping (_ returnError: Error) -> Void) {
+        swifter.getUserFollowers(for: .id(user.id) , cursor: cursor, count: 10, skipStatus: false, includeUserEntities: false, success: { (json, previousCursor, nextCursor) in
+            
+            if let jsonArray = String.convertJsonToArray(json: json) {
+                let followers = TWFollower.from(jsonArray)
+                success(followers!, nextCursor!)
+            }
+        }) { (error) in
+            failure(error)
+        }
+    }
 }
