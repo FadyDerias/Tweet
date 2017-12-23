f3413b7c23823c326697c7b9b851ff9415e42fb2
Fady <fadyderias_92@yahoo.com>
2017-12-24 01:08:35 +0200

    FEAT: Implementing TWFollower object model with implementation of Mappable Protocol to parse JSON objects to it.

--- Tweet/Networking/Models/TWFollower.swift
+++ Tweet/Networking/Models/TWFollower.swift
@@ -7,8 +7,33 @@
 //
 
 import Foundation
+import Mapper
 
-class TWFollower: NSObject, NSCoding {
+class TWFollower : NSObject, NSCoding, Mappable {
     
-  
+    var name: String
+    var handle : String
+    var bio: String
+    var profileImageUrl: String
+    var backgroundImageUrl: String
+    var hasBackgroundImage: Bool
+    
+    init(name: String, handle: String, bio: String, profileImageUrl: String, backgroundImageUrl: String, hasBackgroundImage: Bool) {
+        self.name = name
+        self.handle = handle
+        self.bio = bio
+        self.profileImageUrl = profileImageUrl
+        self.backgroundImageUrl = backgroundImageUrl
+        self.hasBackgroundImage = hasBackgroundImage
+    }
+    
+    //JSON Parsing via Mapper POD
+    required init(map: Mapper) throws {
+         try name = map.from("name")
+         try handle = map.from("screen_name")
+         try bio = map.from("description")
+         try profileImageUrl = map.from("profile_image_url_https")
+         try backgroundImageUrl = map.from("profile_background_image_url_https")
+         try hasBackgroundImage = map.from("profile_use_background_image")
+    }
 }
