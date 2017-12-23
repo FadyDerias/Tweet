194d33252a7e45f4e25175c1d29ba491da47d3ca
Fady <fadyderias_92@yahoo.com>
2017-12-23 18:31:24 +0200

    FEAT: Creating TWFollower model object.

---
+++ Tweet/Networking/Models/TWFollower.swift
@@ -0,0 +1,14 @@
+//
+//  TWFollower.swift
+//  Tweet
+//
+//  Created by Fady on 12/23/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import Foundation
+
+class TWFollower: NSObject, NSCoding {
+    
+  
+}
