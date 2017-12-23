7bae936ed4e58c077842b45e2e4a7cf5db7012c7
Fady <fadyderias_92@yahoo.com>
2017-12-23 18:27:55 +0200

    FEAT: Creating NetworkParameter class/object to hold values of authorization headers keys and their equivalent values.

---
+++ Tweet/Networking/Models/NetworkParameter.swift
@@ -0,0 +1,20 @@
+//
+//  TWHeaderObject.swift
+//  Tweet
+//
+//  Created by Fady on 12/22/17.
+//  Copyright © 2017 Eventtus. All rights reserved.
+//
+
+import Foundation
+
+class NetworkParameter {
+    
+    var key: String
+    var value: String
+    
+    init(key: String, value: String) {
+        self.key = key
+        self.value = value
+    }
+}
