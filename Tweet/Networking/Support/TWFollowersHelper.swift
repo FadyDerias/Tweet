3ceeece804e76eb06eb1fade862dcdb8f3a1b737
Fady <fadyderias_92@yahoo.com>
2017-12-24 10:11:05 +0200

    FEAT: Creating functions in TWFollowersHelper to save and retrieve an array of TWFollower objects via NSCoding protocol.

--- Tweet/Networking/Support/TWFollowersHelper.swift
+++ Tweet/Networking/Support/TWFollowersHelper.swift
@@ -11,6 +11,31 @@ import Foundation
 class TWFollowersHelper {
     
     static let sharedInstance = TWFollowersHelper()
+    private let userFollowersPath = "user_followers"
+    private let documentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first! //Document Directory
     
+    
+    //MARK: - Followers
+    
+    func saveUserFollowersToDocumentsDirectory(followers: [TWFollower]) {
+        let archiveUrl = documentsDirectory.appendingPathComponent(userFollowersPath)
+        
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(followers, toFile: archiveUrl.path)
+        
+        if (!isSuccessfulSave) {
+            print("Failed to Cache User followers")
+        }
+    }
+    
+    func retrieveUserFollowersFromDocumentsDirectory() -> [TWFollower] {
+        let archiveUrl = documentsDirectory.appendingPathComponent(userFollowersPath)
+        
+        if let followers = NSKeyedUnarchiver.unarchiveObject(withFile: archiveUrl.path) as? [TWFollower] {
+            return followers
+        } else {
+            let followers = [TWFollower]()
+            return followers
+        }
+    }
     
 }
